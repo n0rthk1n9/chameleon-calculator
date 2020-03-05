3ffe10b36e51a69c21115e29ecaea133f797f71c
Jan Armbrust <jan.armbrust@gmx.de>
2020-03-05 22:09:09 +0100

    add calculation functions and buttons

--- App.js
+++ App.js
@@ -1,10 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Display from './src/components/Display';
+import Button from './src/components/Button';
+
+let firstOperand;
+let secondOperand;
+let result;
 
 const App = () => {
+  const [display, setDisplay] = useState(0);
+  const [operation, setOperation] = useState('');
+  const [shouldConcatenateDigit, setShouldConcatenateDigit] = useState(false);
+
+  const concatenateDigit = digit => {
+    if (shouldConcatenateDigit) {
+      setDisplay(display + digit);
+    } else {
+      setDisplay(digit);
+      setShouldConcatenateDigit(true);
+    }
+  };
+
+  const activateOperation = operation => {
+    firstOperand = Number(display);
+    setShouldConcatenateDigit(false);
+    setOperation(operation);
+  };
+
+  const generateResult = () => {
+    secondOperand = Number(display);
+
+    switch (operation) {
+      case 'division':
+        result = firstOperand / secondOperand;
+        break;
+      case 'multiplication':
+        secondOperand = Number(display);
+        result = firstOperand * secondOperand;
+        break;
+      case 'subtraction':
+        secondOperand = Number(display);
+        result = firstOperand - secondOperand;
+        break;
+      case 'addition':
+        secondOperand = Number(display);
+        result = firstOperand + secondOperand;
+        break;
+      default:
+        return null;
+    }
+
+    setDisplay(+result.toFixed(5));
+    setOperation('');
+    setShouldConcatenateDigit(false);
+  };
+
+  const cancelButton = () => {
+    if (!shouldConcatenateDigit && display === 0) {
+      setOperation('');
+    }
+    setDisplay(0);
+    setShouldConcatenateDigit(false);
+  };
+
+  const addDot = () => {
+    if (Math.round(display) === Number(display)) {
+      setDisplay(`${display}.`);
+      setShouldConcatenateDigit(true);
+    }
+  };
+
+  const percentage = () => {
+    setDisplay(Number(display) / 100);
+  };
+
+  const invertSignal = () => {
+    setDisplay(Number(display) * -1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Display display={display} />
+      <Button label="1" operation={() => concatenateDigit('1')} />
+      <Button label="2" operation={() => concatenateDigit('2')} />
+      <Button label="3" operation={() => concatenateDigit('3')} />
+      <Button label="4" operation={() => concatenateDigit('4')} />
+      <Button label="5" operation={() => concatenateDigit('5')} />
+      <Button label="6" operation={() => concatenateDigit('6')} />
+      <Button label="7" operation={() => concatenateDigit('7')} />
+      <Button label="8" operation={() => concatenateDigit('8')} />
+      <Button label="9" operation={() => concatenateDigit('9')} />
+      <Button label="0" operation={() => concatenateDigit('0')} />
+      <Button label="+" operation={() => activateOperation('addition')} />
+      <Button
+        label="-"
+        operation={() => activateOperation('subtraction')}
+      />
+      <Button
+        label="*"
+        operation={() => activateOperation('multiplication')}
+      />
+      <Button label="/" operation={() => activateOperation('division')} />
+      <Button label="=" operation={() => generateResult()} />
+      <Button label="." operation={() => addDot()} />
+      <Button label="%" operation={() => percentage()} />
+      <Button label="+/-" operation={() => invertSignal()} />
+      <Button
+        label={display ? 'C' : 'AC'}
+        operation={() => cancelButton()}
+      />
     </View>
   );
 };
